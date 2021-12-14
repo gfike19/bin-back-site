dec99b30eac7a211d07948a4a76b93716fbcaefd
gfike19 <gfike19@gmail.com>
2021-12-14 16:04:09 -0600

    am able to display page on site

--- app.py
+++ app.py
@@ -4,90 +4,9 @@ from PIL import Image
 from PIL import ImageDraw
 import tempfile
 import io
+import base64
 
 app = Flask(__name__)
-
-@app.route("/", methods=['GET'])
-def indexGet():
-    return render_template("index.html")
-
-@app.route("/", methods=['POST'])
-def indexPost():
-    try:
-        # filename will be generated from msg
-        msg = request.form['msg'] + " "
-        fileName = msg + ""
-        fontSz = int(request.form['fontSz'])
-        txtColor = request.form['txtColor']
-        font = "arial"
-
-        res = request.form["res"]
-        arr = res.split(", ")
-        wid = int(arr[0])
-        ht = int(arr[1])
-        imageSz = (wid, ht)
-
-        font = ImageFont.truetype(font, fontSz)
-        bckColor = request.form['bckColor']
-
-        # TODO check to see if text color and background color are the same
-
-        # additional options
-        textDirection = request.form['dir']
-        binary = bool(request.form.getlist('bin'))
-
-        # TODO add capability to rotate image
-        # rot = request.form['rot']
-
-        # technical options
-        # wid = int(request.form['wid'])
-        # ht = int(request.form['ht'])
-
-        extension = request.form['ext']
-        extDict = {
-            ".bmp" : "bmp", ".gif" : "gif", ".jpg": "jpeg", ".tiff": "tiff"
-        }
-        fileFormat = extDict[extension]
-        
-        # create image object to use in functions
-        img = Image.new("RGB", imageSz, bckColor)
-        draw = ImageDraw.Draw(img)
-
-        if binary == True:
-            msg = getBinMsg(msg)
-
-        # mutates img
-        if textDirection == "rtl":
-            rtl(draw, msg, imageSz, txtColor, font, fontSz)
-        
-        if textDirection == "ltr":
-            ltr(draw, msg, imageSz, txtColor, font, fontSz)
-        
-        if textDirection == "ttb":
-            ttb(draw, msg, imageSz, txtColor, font, fontSz)
-        
-        if textDirection == "btt":
-            btt(draw, msg, imageSz, txtColor, font, fontSz)
-
-        # converts image to binary, writes bytes to array which is sent to a temporary file, puts array at the begining of
-        # file before it is returned to user so stream works properly
-        img_byte_arr = io.BytesIO()
-        img.save(img_byte_arr, format=fileFormat)
-        img_byte_arr = img_byte_arr.getvalue()
-        fp = tempfile.TemporaryFile()
-        fp.write(img_byte_arr)
-        fp.seek(0,0)
-
-        return Flask.send_file(fp, as_attachment=False, mimetype=".jpg", attachment_filename=fileName + extension)
-        # below doesn't work
-        # return fp
-        # below doesn't work
-        # return render_template("disp-img.html", img=img)
-    except Exception as e:
-        return str(e)
-
-if __name__ == "__main__":
-    app.run()
 
 def getBinMsg(msg):
     lst = []
@@ -137,3 +56,77 @@ def btt(draw, msg, img_size, txt_color, font, text_size):
             idx -= 1
             if idx == 0:
                 idx = len(msg)
+
+@app.route("/", methods=['GET'])
+def indexGet():
+    return render_template("index.html")
+
+@app.route("/", methods=['POST'])
+def indexPost():
+    try:
+        # filename will be generated from msg
+        msg = request.form['msg'] + " "
+        fileName = msg + ""
+        fontSz = int(request.form['fontSz'])
+        txtColor = request.form['txtColor']
+        font = "arial"
+
+        res = request.form["res"]
+        arr = res.split(", ")
+        wid = int(arr[0])
+        ht = int(arr[1])
+        imageSz = (wid, ht)
+
+        font = ImageFont.truetype(font, fontSz)
+        bckColor = request.form['bckColor']
+
+        # TODO check to see if text color and background color are the same
+
+        # additional options
+        textDirection = request.form['dir']
+        binary = bool(request.form.getlist('bin'))
+
+        # TODO add capability to rotate image
+        # rot = request.form['rot']
+
+        # TODO add ability to do custom size
+        # wid = int(request.form['wid'])
+        # ht = int(request.form['ht'])
+
+        extension = request.form['ext']
+        extDict = {
+            ".bmp" : "bmp", ".gif" : "gif", ".jpg": "jpeg", ".tiff": "tiff"
+        }
+        fileFormat = extDict[extension]
+        
+        # create image object to use in functions
+        img = Image.new("RGB", imageSz, bckColor)
+        draw = ImageDraw.Draw(img)
+
+        if binary == True:
+            msg = getBinMsg(msg)
+
+        # mutates img
+        if textDirection == "rtl":
+            rtl(draw, msg, imageSz, txtColor, font, fontSz)
+        
+        if textDirection == "ltr":
+            ltr(draw, msg, imageSz, txtColor, font, fontSz)
+        
+        if textDirection == "ttb":
+            ttb(draw, msg, imageSz, txtColor, font, fontSz)
+        
+        if textDirection == "btt":
+            btt(draw, msg, imageSz, txtColor, font, fontSz)
+
+        # converts image to binary, writes bytes to array so it can be displayed on site
+        img_byte_arr = io.BytesIO()
+        img.save(img_byte_arr, format=fileFormat)
+        encoded_img_data = base64.b64encode(img_byte_arr.getvalue())
+
+        return render_template("index.html", img_data=encoded_img_data.decode('utf-8'))
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    app.run()
